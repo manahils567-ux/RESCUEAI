be0b84c72c76f70f36570f23d5dce4484b9a9378
HusssainBot4 <hussainahmedgoat@gmail.com>
2026-05-09 12:50:48 +0500

    Fixed cron issue

--- server/index.js
+++ server/index.js
@@ -15,7 +15,19 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://192.168.100.50:3000'],
+  origin: function(origin, callback) {
+    const allowed = [
+      'http://localhost:3000',
+      'http://192.168.100.50:3000',
+      process.env.FRONTEND_URL,           // add this
+    ].filter(Boolean);
+    
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS blocked: ' + origin));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
