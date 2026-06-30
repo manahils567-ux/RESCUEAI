809f57d29f847e8ad8cf34b6359763844129b3e0
Rameesha Akram <akramrameesha2993@gmail.com>
2026-07-01 03:04:37 +0500

    fix: downgrade react-leaflet to v4, replace Geist font with Inter/Roboto Mono for Next 14 compatibility and removed Satellite Intel Component'

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,12 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
